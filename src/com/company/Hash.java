8c4aee5c51bf15a808f8dcd7fa4e6977abe0928f
ArmandoSantiago <2018.armando.santiago@gmail.com>
2019-11-28 12:54:53 -0600

    Para explicar

--- src/com/company/Hash.java
+++ src/com/company/Hash.java
@@ -12,13 +12,44 @@ public class Hash {
             byte[] array = messageDigest.digest(txt.getBytes());
             StringBuffer stringBuffer = new StringBuffer();
             for (int i = 0; i < array.length; ++i) {
-                stringBuffer.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+                stringBuffer.append(Integer.toHexString((array[i] & 0xFF) | 0x100));
             }
             return stringBuffer.toString();
         } catch (NoSuchAlgorithmException e) {
             System.out.println(e.getMessage());
         }
         return null;
+    }
+
+    public static String obtenerSHA(String texto) {
+        try {
+            System.out.println("Incia la conversion de " + texto);
+            System.out.println("**********************************************************");
+            byte [] textoBytes = texto.getBytes();
+            for (int i = 0; i < textoBytes.length ; i++) {
+                System.out.println("Texto en bytes " + i + " es " + textoBytes[i]);
+            }
+            System.out.println("**********************************************************");
+
+            StringBuffer stringBuffer = new StringBuffer();
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+            byte[] array = messageDigest.digest(textoBytes);
+            for (int i = 0; i < array.length; i++) {
+//                System.out.println("Iteracion " + i + " es " + array[i]);
+                System.out.println("Iteracion  de Integer" + i + " es " + Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+                stringBuffer.append(Integer.toHexString((array[i] & 0xFF) | 0x100));
+
+            }
+            System.out.println("**********************************************************");
+
+
+            return stringBuffer.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        String desencriptado = "Hola asdfghjkllñjkzs5f56s43f5667saf\ndsghkjsdhjksdgfhsjadgfhjsdfhjsagfidsugfiusadgflsga";
-        String encriptado = Hash.getHashSHA(desencriptado);
-        System.out.println(desencriptado);
-        System.out.println(encriptado);
-        if (Hash.getHashSHA(desencriptado).equals(encriptado))
-            System.out.println("Son iguales");
+//        Scanner scanner = new Scanner(System.in);
+//        String encriptado = Hash.getHashSHA("Hola");
+//
+//        System.out.println("Ingresa una palabra para verificar");
+//        while (!Hash.getHashSHA(scanner.next()).equals(encriptado)) {
+//            System.out.println("Lo sentimos no funciono, ingresa otra palabra para verificar");
+//        }
+//        System.out.println("Felicidades las palabras coinciden");
+        System.out.println(Hash.obtenerSHA("hola"));
     }
 }
+//[B@74a14482
+//12d11119c15415013b11511c1d115214f1d517f17b19c10e1761d51b81bc
+//19918010b1851d313813e13a12f1b415e1b71d010616a1481791a91da1d0
